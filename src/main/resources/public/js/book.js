aea8cce7ee784a2602d10f3ac8a50189b294e5a3
QiangLiIS <qil45@pitt.edu>
2016-04-03 16:15:00 -0400

    a

--- src/main/resources/public/js/book.js
+++ src/main/resources/public/js/book.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
-	var tool={
-		modify_css:function(object,attr,value){ //设置css的封装函数 （object：对象，attr：要改变的css属性，value：改变的值）
-			object.css(attr,value);
-		},
-		modify_animate:function(object,attr,value,duration,fun){ //设置左右欢动效果的封装函数 （object：对象，attr：要改变的css属性，value：改变的值）
-				object.animate({height : value},duration,function(){fun});
-			},
-		modify_event:function(object,event,fun){ //设置事件的封装函数 （object：对象，event：事件名称，fun：触发的函数）
-				object.bind(event,fun);
-			}
-	}
+
 
 	jQuery.ajax({
 	  url: '/api/timeline_info',
@@ -41,20 +31,20 @@ $(document).ready(function() {
 	$('.searchoption').bind('click', function(event) {
 		$('.searchoption').find('ul').animate({
 			height: 120},
-			200)
+			200);
 		event.stopPropagation(); 
 	});
 
 	$(document).bind('click', function(event) {
 		$('.searchoption').find('ul').animate({
 			height: 0},
-			200)
+			200);
 	});
 
 	$('.searchoption').find('li').bind('click', function(event) {
 		$('.searchoption').find('ul').animate({
 			height: 0},
-			200)
+			200);
 		$('.searchoption').find('label').text($(this).text());
 		event.stopPropagation(); 
 	});
@@ -62,15 +52,15 @@ $(document).ready(function() {
 	$('.strecth').bind('click', function(event) {
 		$('nav').animate({
 			width: 250},
-			200)
+			200);
 		$('nav ul').animate({
 			width: 250},
-			200)
+			200);
 		$('nav ul li').animate({
 			width: 248},
-			200)
+			200);
 		$('.strecth').animate({
 			left: 220},
-			200)
+			200);
 	});
 });
